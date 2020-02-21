984bddf7565979164a78ff5bf881d507754051bb
Vitaliy Dudarevich <v.dudarevich@godeltech.com>
2020-02-21 11:47:13 +0300

    test

---
+++ src/test/java/pages/SearchResultPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+
+import org.openqa.selenium.By;
+
+public class SearchResultPage {
+    private By searchQuery = By.xpath("//div[@class='visible-lg init-subscriber facets-subscriber sorts-subscriber m-sorts-subscriber']/p[@class='m-search display-inline-block']");
+    
+}
